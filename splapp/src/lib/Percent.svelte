8b89ee17042e1ee0bc746cb6dc65782d5449b274
bitfexl <54662051+bitfexl@users.noreply.github.com>
2023-01-15 00:37:42 +0100

    first version of training

---
+++ splapp/src/lib/Percent.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let total;
+    export let actual;
+</script>
+
+{Math.round((actual / total) * 10000) / 100}%
--- splapp/src/lib/training/Training.svelte
+++ splapp/src/lib/training/Training.svelte
@@ -2,6 +2,8 @@
     import questionIndex from "../questionIndex.json";
     import { getQuestions } from "../QuestionSelector.svelte";
     import LocalStorage from "../LocalStorage.svelte";
+    import Percent from "../Percent.svelte";
+    import QuizQuestion from "../QuizQuestion.svelte";
 
     const allCategories = questionIndex.categories.map((c) => c.id);
 
@@ -19,11 +21,15 @@
         }
     }
 
+    // trays ///////////////////////////////////////////////////////////////////
+
+    let activeTray = -1; // no tray selected
+
     let trays = [allQuestions.map((q) => q.id), [], [], [], []];
 
     function randomQuestion(i) {
         let tray = trays[i];
-        return tray[Math.random() * tray.length];
+        return tray[Math.floor(Math.random() * tray.length)];
     }
 
     function promote(questionId, i) {
@@ -46,34 +52,93 @@
         trays[i + dir].push(questionId);
         trays = [...trays]; // update (svelte)
     }
-</script>
 
-<h1>Lernfächer</h1>
+    function setTray(tray) {
+        activeTray = tray;
+        if (tray != -1) {
+            questionId = randomQuestion(activeTray);
+        }
+    }
+
+    // quiz question ///////////////////////////////////////////////////////////
+
+    let questionId; // active question
+    let showCorrect = false;
+    let selectionCorrect;
+
+    function nextQuestion() {
+        if (!showCorrect) {
+            showCorrect = true;
+            return;
+        }
+        showCorrect = false;
+
+        if (selectionCorrect) {
+            promote(questionId, activeTray);
+        } else {
+            demote(questionId, activeTray);
+        }
+
+        questionId = randomQuestion(activeTray);
+    }
+</script>
 
 <LocalStorage key="trays" bind:val={trays} />
 
-{#each trays as tray, ti}
-    <div class="questions">
-        {#each tray as question}
-            <div>
-                {question}
-                <button on:click={() => promote(question, ti)}>Promote</button>
-                <button on:click={() => demote(question, ti)}>Demote</button>
+<h1>Lernfächer</h1>
+
+<p>So gehts:</p>
+
+{#if activeTray != -1}
+    <div>
+        <h3>Fach {activeTray + 1}</h3>
+        <p>
+            {trays[activeTray].length} Frage{trays[activeTray].length != 1 ? "n" : ""} (<Percent
+                total={allQuestions.length}
+                actual={trays[activeTray].length}
+            />)
+        </p>
+    </div>
+    {#if questionId}
+        <!-- undefined if no question in tray -->
+        <QuizQuestion question={questions[questionId]} bind:correct={selectionCorrect} {showCorrect} />
+        <button on:click={() => nextQuestion()}>Weiter</button>
+    {/if}
+    <br />
+    <button class="linkLike" on:click={() => setTray(-1)}>Zurück zur Lernfächerauswahl</button>
+{:else}
+    <div class="trays">
+        {#each trays as tray, ti}
+            <div class="tray">
+                <div>
+                    <h3>Fach {ti + 1}</h3>
+                    <p>{tray.length} Frage{tray.length != 1 ? "n" : ""} (<Percent total={allQuestions.length} actual={tray.length} />)</p>
+                </div>
+                <div class="start">
+                    <button class="linkLike" on:click={() => setTray(ti)}>Fragen üben</button>
+                </div>
             </div>
         {/each}
     </div>
-{/each}
-
-{JSON.stringify(trays, null, 2)}
+{/if}
 
 <style>
-    h1 {
-        color: red;
+    .trays {
+        display: flex;
+        flex-direction: column-reverse;
+        gap: 20px;
+        width: 600px; /* fix for mobile */
     }
 
-    .questions {
+    .tray {
         display: grid;
-        grid-template-columns: repeat(7, 1fr);
-        padding-bottom: 40px;
+        grid-template-columns: 1fr 1fr;
+        padding: 10px;
+        background-color: whitesmoke;
+        box-shadow: 0 0 2px gray;
+    }
+
+    .start {
+        display: flex;
     }
 </style>
